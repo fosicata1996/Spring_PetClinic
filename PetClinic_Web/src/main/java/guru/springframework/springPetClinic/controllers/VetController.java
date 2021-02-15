c79f6ef92df769c8831de47a0fa8dcdf9ab629e3
fosicata1996 <fosalaucatalin@gmail.com>
2021-02-15 22:35:44 +0200

    List vets. Closes #15

--- PetClinic_Web/src/main/java/guru/springframework/springPetClinic/controllers/VetController.java
+++ PetClinic_Web/src/main/java/guru/springframework/springPetClinic/controllers/VetController.java
@@ -1,14 +1,26 @@
 package guru.springframework.springPetClinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.springPetClinic.services.VetService;
 
 @Controller
 public class VetController
 {
-	@RequestMapping({ "/vets", "/vets/index", "/vets/index.html" })
-	public String listVets()
+	private final VetService vetService;
+	
+	public VetController(VetService vetService)
 	{
+		this.vetService = vetService;
+	}
+	
+	@RequestMapping({ "/vets", "/vets/index", "/vets/index.html" })
+	public String listVets(Model model)
+	{
+		model.addAttribute("vets", vetService.findAll());
+		
 		return "vets/index";
 	}
 }
