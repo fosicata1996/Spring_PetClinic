b2ff093b8c295871ed39acf82f4cbf4473d555b2
fosicata1996 <fosalaucatalin@gmail.com>
2021-03-08 20:43:07 +0200

    Create Visit Map Service. Closes #46

---
+++ PetClinic_Data/src/main/java/guru/springframework/springPetClinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package guru.springframework.springPetClinic.services;
+
+import guru.springframework.springPetClinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>
+{
+}
